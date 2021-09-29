5830fcf9e2a6b3838ba55c134bda245dd233bcb5
Bressan <marcos.bressan@bigdata.inf.br>
2021-09-29 21:27:52 +0000

    refactor: Statements

---
+++ deirokay/statements.py
@@ -0,0 +1,40 @@
+
+
+class Statement:
+    def __init__(self, stmt_options: dict):
+        self.options = stmt_options
+
+    def __call__(self, df):
+        internal_report = self.report(df)
+        result = self.result(internal_report)
+
+        final_report = {
+            'detail': internal_report,
+            'result': 'pass' if result else 'fail'
+        }
+        return final_report
+
+    def report(self, df):
+        pass
+
+    def result(self, report):
+        pass
+
+
+class Unique(Statement):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.at_least_perc = self.options.get('at_least_%', 100.0)
+
+    def report(self, df):
+        unique = ~df.duplicated(keep=False)
+
+        report = {
+            'unique_rows': int(unique.sum()),
+            'unique_rows_%': float(100.0*unique.sum()/len(unique)),
+        }
+        return report
+
+    def result(self, report):
+        return report.get('unique_rows_%') > self.at_least_perc
--- deirokay/validator.py
+++ deirokay/validator.py
@@ -1,55 +1,17 @@
 import json
 from pprint import pprint
 
+import deirokay.statements as core_stmts
+
 from .exceptions import ValidationError
-
-
-class Statement:
-    def __init__(self, stmt_options: dict):
-        self.options = stmt_options
-
-    def __call__(self, df):
-        internal_report = self.report(df)
-        result = self.result(internal_report)
-
-        final_report = {
-            'detail': internal_report,
-            'result': 'pass' if result else 'fail'
-        }
-        return final_report
-
-    def report(self, df):
-        pass
-
-    def result(self, report):
-        pass
-
-
-class Unique(Statement):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.at_least_perc = self.options.get('at_least_%', 100.0)
-
-    def report(self, df):
-        unique = ~df.duplicated(keep=False)
-
-        report = {
-            'unique_rows': int(unique.sum()),
-            'unique_rows_%': float(100.0*unique.sum()/len(unique)),
-        }
-        return report
-
-    def result(self, report):
-        return report.get('unique_rows_%') > self.at_least_perc
 
 
 def _process_stmt(statement):
     statement = statement.copy()
     stmts_map = {
-        'unique': Unique,
+        'unique': core_stmts.Unique,
     }
-    stmt_type: Statement = statement.get('type')
+    stmt_type: core_stmts.Statement = statement.get('type')
     try:
         return stmts_map[stmt_type](statement)
     except KeyError:
