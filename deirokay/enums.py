52a037f492b40ae7dc02aa599b79b58488b14f62
Bressan <marcos.bressan@bigdata.inf.br>
2021-09-30 19:51:56 +0000

    feat: More aliases for dtypes

--- deirokay/enums.py
+++ deirokay/enums.py
@@ -3,7 +3,9 @@ from enum import Enum
 
 class DTypes(str, Enum):
     INT64 = 'integer'
+    INTEGER = INT64
     FLOAT64 = 'float'
+    FLOAT = FLOAT64
     STRING = 'string'
     STR = STRING
     DATETIME = 'datetime'
