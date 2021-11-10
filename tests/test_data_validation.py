47608a892f4b482114e84c538edec6b4f9ec0a8f
Bressan <marcos.bressan@bigdata.inf.br>
2021-11-10 21:36:52 +0000

    tests: refactor test_statements

--- tests/test_data_validation.py
+++ tests/test_data_validation.py
@@ -52,39 +52,6 @@ def test_data_validation_from_json():
     validate(df, against='tests/assertions.json')
 
 
-def test_not_null_statement():
-    df = data_reader(
-        'tests/transactions_sample.csv',
-        options='tests/options.json'
-    )
-
-    assertions = {
-        'items': [
-            {
-                'scope': 'NUM_TRANSACAO01',
-                'statements': [
-                    {
-                        'type': 'not_null',
-                        'at_least_%': 90,
-                    }
-                ]
-            },
-            {
-                'scope': ['WERKS01', 'HR_TRANSACAO01'],
-                'statements': [
-                    {
-                        'type': 'not_null',
-                        'at_least_%': 90.0,
-                        'multicolumn_logic': 'any'
-                    }
-                ]
-            },
-        ]
-    }
-
-    validate(df, against=assertions)
-
-
 def test_custom_statement():
     df = data_reader(
         'tests/transactions_sample.csv',
