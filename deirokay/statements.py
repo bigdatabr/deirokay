7c343095fb32a25015490c34b0ca9e45ee544354
Ubuntu <ubuntu@ip-10-0-1-214.ec2.internal>
2021-12-10 13:23:30 +0000

    syntax fix

--- deirokay/statements.py
+++ deirokay/statements.py
@@ -287,7 +287,7 @@ class RowCount(BaseStatement):
 
 
 class Regex(BaseStatement):
-    """Check if the rows of a scoped DataFrame obey to certain regex pattern."""
+    """Check if the rows of a scoped DataFrame obey to a regex pattern."""
 
     name = 'regex'
     expected_parameters = ['pattern', 'at_least_%']
@@ -299,24 +299,27 @@ class Regex(BaseStatement):
         self.at_least_perc = self.options.get('at_least_%', 100.0)
 
     def report(self, df):
-        
+
         report = {}
-    
+
         for column in df.columns:
-            matches = df[column].apply(lambda x: bool(re.fullmatch(self.pattern, str(x))))
+            matches = df[column].apply(lambda x: bool(
+                                       re.fullmatch(self.pattern, str(x))))
             report[column] = {
                 'matching_rows': int(matches.sum()),
                 'matching_rows_%': float(100*matches.sum()/len(matches)),
                 'not_matching_rows': int((~matches).sum()),
-                'not_matching_rows_%': float(100.0*(~matches).sum()/len(matches)),
+                'not_matching_rows_%': float(100.0*(~matches).sum(
+                                                              )/len(matches)),
             }
-            
+
         # print('report:\n', pd.DataFrame(report).to_markdown())
-        
+
         return report
 
     def result(self, report):
         for col_report in report:
-            if not report[col_report].get('matching_rows_%') >= self.at_least_perc:
+            if not report[col_report].get('matching_rows_%'
+                                          ) >= self.at_least_perc:
                 return False
         return True
