a9e3ae07549721ab44bedbb912f3dda9d136b6bb
Marcus Vinicius Laia de Souza <marcus.laia@bigdata.inf.br>
2021-12-13 14:51:22 +0000

    Sort imports of __init__.py

--- deirokay/statements/__init__.py
+++ deirokay/statements/__init__.py
@@ -5,9 +5,9 @@ Module for BaseStatement and builtin Deirokay statements.
 from .base_statement import BaseStatement
 from .contain import Contain
 from .not_null import NotNull
+from .regex import Regex
 from .row_count import RowCount
 from .unique import Unique
-from .regex import Regex
 
 __all__ = (
     'BaseStatement',
