c9c4ab9e07745afdf742d73ec5bcd56fb6a772e6
Rafael Higa <rafael.higa@bigdata.com.br>
2022-02-03 18:34:05 +0000

    Resolve "Statement "equation" for column values"

--- deirokay/statements/__init__.py
+++ deirokay/statements/__init__.py
@@ -3,6 +3,7 @@ Module for BaseStatement and builtin Deirokay statements.
 """
 
 from .base_statement import BaseStatement
+from .column_expression import ColumnExpression
 from .contain import Contain
 from .not_null import NotNull
 from .row_count import RowCount
@@ -10,6 +11,7 @@ from .unique import Unique
 
 __all__ = (
     'BaseStatement',
+    'ColumnExpression',
     'Contain',
     'NotNull',
     'RowCount',
