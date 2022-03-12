9d250e5688b59078580e0582b5edebaa6b2cb9b9
Marcos Bressan <bressan@dee.ufc.br>
2022-03-11 23:14:58 -0300

    fix links

--- deirokay/enums.py
+++ deirokay/enums.py
@@ -9,31 +9,39 @@ class DTypes(str, Enum):
     """Deirokay data types."""
 
     INT64 = 'integer'
-    """Integer values, treated by :ref:`IntegerTreater<deirokay.parser.treaters.IntegerTreater>`."""
+    """Integer values, treated by
+    :ref:`IntegerTreater<deirokay.parser.treaters.IntegerTreater>`."""
     INTEGER = INT64
     """Alias for INT64."""
     FLOAT64 = 'float'
-    """Float values, treated by :ref:`FloatTreater<deirokay.parser.treaters.FloatTreater>`."""
+    """Float values, treated by
+    :ref:`FloatTreater<deirokay.parser.treaters.FloatTreater>`."""
     FLOAT = FLOAT64
     """Alias for FLOAT64."""
     STRING = 'string'
-    """Text values, treated by :ref:`StringTreater<deirokay.parser.treaters.StringTreater>`."""
+    """Text values, treated by
+    :ref:`StringTreater<deirokay.parser.treaters.StringTreater>`."""
     STR = STRING
     """Alias for STRING."""
     DATETIME = 'datetime'
-    """Datetime values, treated by :ref:`DateTime64Treater<deirokay.parser.treaters.DateTime64Treater>`."""
+    """Datetime values, treated by
+    :ref:`DateTime64Treater<deirokay.parser.treaters.DateTime64Treater>`."""
     DT = DATETIME
     """Alias for DATETIME."""
     DATE = 'date'
-    """Date values, treated by :ref:`DateTreater<deirokay.parser.treaters.DateTreater>`."""
+    """Date values, treated by
+    :ref:`DateTreater<deirokay.parser.treaters.DateTreater>`."""
     TIME = 'time'
-    """Time values, treated by :ref:`TimeTreater<deirokay.parser.treaters.TimeTreater>`."""
+    """Time values, treated by
+    :ref:`TimeTreater<deirokay.parser.treaters.TimeTreater>`."""
     BOOLEAN = 'boolean'
-    """Boolean values, treated by :ref:`BooleanTreater<deirokay.parser.treaters.BooleanTreater>`."""
+    """Boolean values, treated by
+    :ref:`BooleanTreater<deirokay.parser.treaters.BooleanTreater>`."""
     BOOL = BOOLEAN
     """Alias for BOOLEAN."""
     DECIMAL = 'decimal'
-    """Decimal values, treated by :ref:`DecimalTreater<deirokay.parser.treaters.DecimalTreater>`."""
+    """Decimal values, treated by
+    :ref:`DecimalTreater<deirokay.parser.treaters.DecimalTreater>`."""
 
 
 class SeverityLevel(int, Enum):
