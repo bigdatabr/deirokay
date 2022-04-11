f7177d72733e6ebc307ba9b31e33262f0ad5f516
Marcos Bressan <bressan@dee.ufc.br>
2022-04-11 15:55:14 -0300

    feat: Statistic in Internval statement

--- deirokay/statements/builtin/__init__.py
+++ deirokay/statements/builtin/__init__.py
@@ -8,6 +8,7 @@ from .column_expression import ColumnExpression  # noqa: F401
 from .contain import Contain  # noqa: F401
 from .not_null import NotNull  # noqa: F401
 from .row_count import RowCount  # noqa: F401
+from .statistic_in_interval import StatisticInInterval  # noqa F401
 from .unique import Unique  # noqa: F401
 
 __all__ = tuple(
