d557f4bfdd84b2083f62fe0fc8c1cd6093b4a564
Marcos Alves de Almeida <35045860+almeida-marcos@users.noreply.github.com>
2024-04-11 09:05:06 -0300

    feat: Add "tolerance_%" parameter in Contain statement (#61)
    Co-authored-by: Marcos Bressan <marcos.bressan@bigdata.com.br>

--- deirokay/statements/builtin/contain.py
+++ deirokay/statements/builtin/contain.py
@@ -48,6 +48,11 @@ class Contain(BaseStatement):
       rules, 0 for `only`.
     * `max_occurrences`: a global maximum number of occurrences for
       each of the `values`. Default: `inf` (unbounded).
+    * `tolerance_%`: The allowed percentage tolerance when using the rule
+      `only` and `all`. For example, if you define an `only` rule with the
+      values `["a", "b", "c"], but value "d" was also in the df, with a 25%
+      toleranace, the validation will pass, that is, at most 25% of the
+      unique values on the df weren't on the `values` list.
     * `occurrences_per_value`: a list of dictionaries overriding the
       global boundaries. Each dictionary may have the following keys:
 
@@ -87,7 +92,7 @@ class Contain(BaseStatement):
     are sure that they will be always present.
 
     The `min_occurrences` and `max_occurrences` parameters are applied
-    applied to all the `values` declared, and only these. It means you
+    to all the `values` declared, and only these. It means you
     cannot (yet) specify boundaries for values you did't declare.
 
     Null values are considered valid for the purpose of the statement
@@ -124,8 +129,8 @@ class Contain(BaseStatement):
         }
 
     * You have a table of servers containg a column `role` which may
-      contain the values `master` and `slave`.
-      You want to be sure that there is always one and only one master
+      contain the values `controller` and `worker`.
+      You want to be sure that there is always one and only one controller
       server in the data.
 
     .. code-block:: json
@@ -136,7 +141,7 @@ class Contain(BaseStatement):
                 {
                     "type": "contain",
                     "rule": "all",
-                    "values": ["master"],
+                    "values": ["controller"],
                     "parser": {"dtype": "string"},
                     "min_occurrences": 1,
                     "max_occurrences": 1
@@ -145,7 +150,7 @@ class Contain(BaseStatement):
         }
 
     You may also extend the previous example by making some adjustments
-    to ensure that there is no other value than `master` and `role`
+    to ensure that there is no other value than `controller` and `worker`
     in the data. Make notice that although the `rule` below is changed
     to `only`, the statement above is still contemplated by the
     `occurrences_per_value` parameter in the following validation item:
@@ -158,11 +163,11 @@ class Contain(BaseStatement):
                 {
                     "type": "contain",
                     "rule": "only",
-                    "values": ["master", "slave"],
+                    "values": ["controller", "worker"],
                     "parser": {"dtype": "string"},
                     "occurrences_per_value": [
                         {
-                            "values": ["master"],
+                            "values": ["controller"],
                             "min_occurrences": 1,
                             "max_occurrences": 1
                         }
@@ -237,17 +242,12 @@ class Contain(BaseStatement):
         "parsers",
         "min_occurrences",
         "max_occurrences",
+        "tolerance_%",
         "occurrences_per_value",
         "report_limit",
     ]
     supported_backends: List[Backend] = [Backend.PANDAS, Backend.DASK]
 
-    DEFAULT_MIN_OCCURRENCES = {"all": (1, 0), "only": (0, 0), "all_and_only": (1, 0)}
-    DEFAULT_MAX_OCCURRENCES = {
-        "all": (numpy.inf, numpy.inf),
-        "only": (numpy.inf, 0),
-        "all_and_only": (numpy.inf, 0),
-    }
     DEFAULT_REPORT_LIMIT = 32
 
     def __init__(self, *args, **kwargs) -> None:
@@ -255,6 +255,11 @@ class Contain(BaseStatement):
 
         self.rule = self.options["rule"]
         assert self.rule in ("all", "only", "all_and_only")
+        self.tolerance_perc = self.options.get("tolerance_%", 0)
+        if self.tolerance_perc > 0 and self.rule == "all_and_only":
+            raise ValueError(
+                "It's not possible to set a tolerance_perc with the rule 'all_and_only'"
+            )
         self.multicolumn = self.options.get("multicolumn", False)
         _parsers = self.options.get("parser") or self.options["parsers"]
         if self.multicolumn:
@@ -266,28 +271,10 @@ class Contain(BaseStatement):
             for parser in self.parsers
         ]
 
-        self.min_occurrences = self.options.get("min_occurrences", None)
-        self.max_occurrences = self.options.get("max_occurrences", None)
+        self.min_occurrences = self.options.get("min_occurrences", 0)
+        self.max_occurrences = self.options.get("max_occurrences", numpy.inf)
         self.occurrences_per_value = self.options.get("occurrences_per_value", [])
         self.report_limit = self.options.get("report_limit", NODEFAULT)
-
-        self._set_default_minmax_occurrences()
-
-    def _set_default_minmax_occurrences(self) -> None:
-        final_min = noneor(
-            self.min_occurrences, Contain.DEFAULT_MIN_OCCURRENCES[self.rule][0]
-        )
-        final_max = noneor(
-            self.max_occurrences, Contain.DEFAULT_MAX_OCCURRENCES[self.rule][0]
-        )
-        if self.max_occurrences is not None and self.max_occurrences < final_min:
-            final_min = self.max_occurrences
-        if self.min_occurrences is not None and self.min_occurrences > final_max:
-            final_max = self.min_occurrences
-        assert final_min >= 0
-        assert final_max >= 0
-        self.min_occurrences = final_min
-        self.max_occurrences = final_max
 
     def _generate_analysis(self, value_counts):
         if self.multicolumn:
@@ -326,6 +313,7 @@ class Contain(BaseStatement):
         occurrence_limits.drop_duplicates(
             subset=value_counts.index.names, keep="first", inplace=True
         )
+        occurrence_limits["in_values"] = True
         occurrence_limits.set_index(value_counts.index.names, inplace=True)
 
         analysis = (
@@ -334,35 +322,18 @@ class Contain(BaseStatement):
             .merge(occurrence_limits.reset_index(), how="outer")
         )
         analysis["count"].fillna(0, inplace=True)
-        analysis["min"].fillna(
-            Contain.DEFAULT_MIN_OCCURRENCES[self.rule][1], inplace=True
-        )
-        analysis["max"].fillna(
-            Contain.DEFAULT_MAX_OCCURRENCES[self.rule][1], inplace=True
-        )
-        analysis["result"] = analysis["count"].ge(analysis["min"]) & analysis[
-            "count"
-        ].le(analysis["max"])
+        analysis["min"].fillna(0, inplace=True)
+        analysis["max"].fillna(numpy.inf, inplace=True)
+        analysis["in_values"].fillna(False, inplace=True)
+        analysis["occurrences_result"] = analysis["count"].ge(
+            analysis["min"]
+        ) & analysis["count"].le(analysis["max"])
         return analysis
 
     def _generate_report(self, analysis):
-        columns = [analysis[col] for col in analysis.columns[:-4]]
-        serialized = (
-            treater.serialize(column) for treater, column in zip(self.treaters, columns)
-        )
-        rows = zip(*(s["values"] for s in serialized))
-        rows = (list(row) for row in rows)
-        values_report = sorted(
-            [
-                {
-                    "value": value_row,
-                    "count": analysis_row.count,
-                    "result": analysis_row.result,
-                }
-                for value_row, analysis_row in zip(rows, analysis.itertuples())
-            ],
-            key=lambda x: x["result"],
-        )
+        values_report = self._create_values_report(analysis)
+        values_report = sorted(values_report, key=lambda x: x["result"])
+        rule_report = self._create_rule_report(analysis)
 
         if (
             self.report_limit is NODEFAULT
@@ -386,8 +357,67 @@ class Contain(BaseStatement):
                 else values_report
                 if self.report_limit is None
                 else values_report[: self.report_limit]
-            )
+            ),
+            "rule_report": rule_report,
         }
+
+    def _create_values_report(self, analysis):
+        columns = [analysis[col] for col in analysis.columns[:-4]]
+        serialized = (
+            treater.serialize(column) for treater, column in zip(self.treaters, columns)
+        )
+        rows = zip(*(s["values"] for s in serialized))
+        rows = (list(row) for row in rows)
+        values_report = [
+            {
+                "value": value_row,
+                "count": analysis_row.count,
+                "result": analysis_row.occurrences_result,
+            }
+            for value_row, analysis_row in zip(rows, analysis.itertuples())
+        ]
+        return values_report
+
+    def _create_rule_report(self, analysis):
+        values_in_df = analysis[(analysis["count"] > 0)]
+        perc_in_values = float(values_in_df["in_values"].mean() * 100)
+
+        in_values = analysis[(analysis["in_values"]) & (analysis["max"] > 0)]
+        if len(in_values):
+            perc_values_in_df = float((in_values["count"] > 0).mean()) * 100
+        else:
+            perc_values_in_df = 0
+
+        if self.rule == "only":
+            result = perc_in_values >= 100 - self.tolerance_perc
+            return {
+                "rule": self.rule,
+                "perc_in_values": perc_in_values,
+                "tolerance_%": self.tolerance_perc,
+                "result": result,
+            }
+        elif self.rule == "all":
+            in_values = analysis[(analysis["in_values"]) & (analysis["max"] > 0)]
+            if len(in_values):
+                result = perc_values_in_df >= 100 - self.tolerance_perc
+                return {
+                    "rule": self.rule,
+                    "perc_values_in_df": perc_values_in_df,
+                    "tolerance_%": self.tolerance_perc,
+                    "result": result,
+                }
+            else:
+                return {
+                    "rule": self.rule,
+                    "result": True,  # All the values have check of not contain
+                }
+        elif self.rule == "all_and_only":
+            return {
+                "rule": self.rule,
+                "perc_in_values": perc_in_values,
+                "perc_values_in_df": perc_values_in_df,
+                "result": perc_in_values == 100 and perc_values_in_df == 100,
+            }
 
     @report(Backend.PANDAS)
     def _report_pandas(self, df: "pandas.DataFrame") -> dict:
@@ -417,7 +447,9 @@ class Contain(BaseStatement):
 
     # docstr-coverage:inherited
     def result(self, report: dict) -> bool:
-        return all(item["result"] for item in report["values"])
+        values_result = all(item["result"] for item in report["values"])
+        rule_result = report["rule_report"]["result"]
+        return values_result and rule_result
 
     @profile(Backend.PANDAS)
     @staticmethod
